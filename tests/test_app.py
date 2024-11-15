2f4d0ca6da8265a5a630b3104bce8d41767975b9
Hippity <zhz07@aub.edu.lb>
2024-11-15 05:44:53 +0300

    added asserts

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,6 +4,10 @@ import unittest
 class TestApp(unittest.TestCase):
     def test_greet(self):
         self.assertEqual(greet("World"), "Hello World, from Zein Zebib!")
+    
+    def test_greet_invalid_input(self):
+        with self.assertRaises(ValueError):
+            greet(123)
 
 if __name__ == "__main__":
     unittest.main()
